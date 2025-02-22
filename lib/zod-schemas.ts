fd65d4c666e6cf6479365e00db3237967b5e86e3
Jerome Doran <disneyfan9184@gmail.com>
2025-02-22 09:24:51 -0500

    added uploadthing file storage library

---
+++ lib/zod-schemas.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const companySchema = z.object({
+  name: z.string().min(2, 'Name must be at least 2 characters long'),
+  location: z.string().min(1, 'Location is required'),
+  about: z
+    .string()
+    .min(10, 'Please provide some information about your company'),
+  logo: z.string().min(1, 'Please upload a logo'),
+  website: z.string().url('Please provide a valid website URL'),
+  xAccount: z.string().optional(),
+});
