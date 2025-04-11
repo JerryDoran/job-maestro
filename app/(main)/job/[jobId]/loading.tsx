37df69d2fd90ab6da09fec88a3a9ff65b1f1ba11
Jerome Doran <disneyfan9184@gmail.com>
2025-04-11 16:53:24 -0400

    added skeleton to job details page

---
+++ app/(main)/job/[jobId]/loading.tsx
@@ -0,0 +1,58 @@
+import { Card } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
+
+export default function LoadingJobPage() {
+  return (
+    <div className='container mx-auto py-8'>
+      <div className='grid lg:grid-cols-3 gap-8'>
+        <div className='space-y-8 col-span-2'>
+          <div className='flex items-start justify-between'>
+            <div className=''>
+              <Skeleton className='w-[300px] h-9 mb-2' />
+              <div className='flex items-center gap-2'>
+                <Skeleton className='w-[120px] h-4' />
+                <Skeleton className='w-[90px] h-4' />
+                <Skeleton className='w-[90px] h-4' />
+              </div>
+            </div>
+          </div>
+          <section className='space-y-4'>
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-3/4 h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-5/6 h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-3/4 h-4' />
+            <Skeleton className='w-full h-4' />
+            <Skeleton className='w-5/6 h-4' />
+            <Skeleton className='w-full h-4' />
+          </section>
+          <section>
+            <Skeleton className='w-[200px] h-6 mb-4' />
+            <div className='flex flex-wrap gap-2'>
+              {[...Array(15)].map((_, index) => (
+                <Skeleton key={index} className='w-[120px] h-8 rounded-full' />
+              ))}
+            </div>
+          </section>
+        </div>
+
+        <div className='space-y-6'>
+          <Card className='p-6'>
+            <div className='space-y-4'>
+              <div className=''>
+                <Skeleton className='w-[100px] h-4 mb-2' />
+                <Skeleton className='w-full h-4 mb-2' />
+                <Skeleton className='w-full h-4' />
+              </div>
+              <Skeleton className='w-full h-10' />
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
 
       {/* Job List */}
       <div className='col-span-2 flex flex-col gap-6'>
-        <JobListingSkeleton />
+        {/* <JobListingSkeleton /> */}
         <Suspense fallback={<JobListingSkeleton />}>
-          {/* <JobListings /> */}
+          <JobListings />
         </Suspense>
       </div>
     </div>
